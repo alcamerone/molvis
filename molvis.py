612245ab932c4f5671d8e29d6e16627160affbfd
Cam Ekblad <al.camerone@gmail.com>
2022-04-25 21:48:52 +1200

    Working

--- molvis.py
+++ molvis.py
@@ -12,24 +12,35 @@ from typing import Union
 class Macromolecule:
     def __init__(self, pdbid: str):
         pdbl = PDBList()
-        # TODO: Clean up after ourselves
         file_path = pdbl.retrieve_pdb_file(pdbid, pdir='.', file_format='pdb')
-        self.molecule = Chem.MolFromPDBFile(file_path)
-        self.molecule = Chem.AddHs(self.molecule)
-        AllChem.EmbedMolecule(self.molecule)
+        with open(file_path, 'r') as pdb_file:
+            # Design decision: copying the file into memory makes it easier to tidy up after ourselves,
+            # as we can delete the downloaded file once we're done.
+            # However, for larger molecules on machines with less memory, it may be advantageous
+            # to leave the files on disk.
+            # I've opted for the tidier version for now.
+            self.pdb_data = pdb_file.read()
+        try:
+            # Tidy up downloaded file
+            os.remove(file_path)
+            # Remove `obsolete` dir, which is created when `PDBList` is instantiated
+            os.rmdir('./obsolete')
+        except Exception as e:
+            # Not the end of the world if this doesn't work.
+            # Don't crash the app, but tell the user about it.
+            print("Exception during cleanup:", e)
 
-    def show(self, existing_viewer: Union[ComponentViewer, None] = None) -> ComponentViewer:
+    def show(self, existing_viewer: Union[ComponentViewer, None] = None) -> Union[ComponentViewer, None]:
         if existing_viewer:
-            file_handle = StringIO(Chem.MolToPDBBlock(self.molecule))
-            existing_viewer.add_component(file_handle, ext='pdb')
-            return existing_viewer
-        return nglview.show_rdkit(self.molecule)
+            existing_viewer.add_component(StringIO(self.pdb_data), ext='pdb')
+            # Do not return the ComponentViewer a second time,
+            # just add to the provided viewer
+            return
+        return nglview.show_file(StringIO(self.pdb_data), ext='pdb')
 
     def save(self, file_path: Union[str, Path]) -> Path:
-        # `Path`s are not accepted as an argument, so we need to ensure the arg is a string
-        writer = Chem.PDBWriter(str(file_path))
-        writer.write(self.molecule)
-        writer.close()
+        with open(file_path, 'w') as outfile:
+            outfile.write(self.pdb_data)
         if type(file_path) == 'Path':
             return file_path
         return Path(file_path)
@@ -37,22 +48,17 @@ class Macromolecule:
 class ChemicalMolecule:
     def __init__(self, chemblid: str):
         mol_structs = chembl.molecule.filter(chembl_id=chemblid).only(['molecule_structures'])
-        tmp_file_name = f'tmp_{chemblid}.rdkit'
-        # This file write is necessary because the argument to `MolFromMolFile` *must* be a string
-        # representing a file name, a file handle is not allowed.
-        tmp = open(tmp_file_name, 'w')
-        tmp.write(mol_structs[0]['molecule_structures']['molfile'])
-        tmp.close()
-        self.molecule = Chem.MolFromMolFile(tmp_file_name)
-        os.remove(tmp_file_name)
+        self.molecule = Chem.MolFromMolBlock(mol_structs[0]['molecule_structures']['molfile'])
         self.molecule = Chem.AddHs(self.molecule)
         AllChem.EmbedMolecule(self.molecule)
 
-    def show(self, existing_viewer: Union[ComponentViewer, None] = None) -> ComponentViewer:
+    def show(self, existing_viewer: Union[ComponentViewer, None] = None) -> Union[ComponentViewer, None]:
         if existing_viewer:
             file_handle = StringIO(Chem.MolToPDBBlock(self.molecule))
             existing_viewer.add_component(file_handle, ext='pdb')
-            return existing_viewer
+            # Do not return the ComponentViewer a second time,
+            # just add to the provided viewer
+            return
         return nglview.show_rdkit(self.molecule)
 
     def save(self, file_path: Union[str, Path]) -> Path:
